9393e8ab9ee052e69333eeb71ab2e3b50d191bb4
Simon Santoro <simon.santoro@konverto.eu>
2023-09-22 11:01:28 +0200

    fix route

--- src/Cloud5mins.ShortenerTools.Functions/Functions/UrlCreate.cs
+++ src/Cloud5mins.ShortenerTools.Functions/Functions/UrlCreate.cs
@@ -48,7 +48,7 @@ namespace Cloud5mins.ShortenerTools.Functions
 
         [Function("UrlCreate")]
         public async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "api/UrlCreate")] HttpRequestData req,
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "UrlCreate")] HttpRequestData req,
             ExecutionContext context
         )
         {
